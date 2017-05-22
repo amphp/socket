0aa64dbf5da8817a4b0a5de946c5af9558b7272c
Niklas Keller <me@kelunik.com>
2017-05-22 17:27:42 +0200

    Refactor SocketPool to be an interface and remove host connection limit

---
+++ lib/RawSocketPool.php
@@ -0,0 +1,203 @@
+<?php
+
+namespace Amp\Socket;
+
+use Amp\Loop;
+use Amp\Promise;
+use Amp\Struct;
+use Amp\Success;
+use function Amp\call;
+
+/** @internal */
+class SocketPoolStruct {
+    use Struct;
+
+    public $id;
+    public $uri;
+    public $resource;
+    public $isAvailable;
+    public $idleWatcher;
+    public $idleTimeout;
+}
+
+class RawSocketPool implements SocketPool {
+    private $sockets = [];
+    private $socketIdUriMap = [];
+    private $pendingCount = [];
+
+    private $options = [
+        self::OP_IDLE_TIMEOUT => 10000,
+        self::OP_CONNECT_TIMEOUT => 10000,
+        self::OP_BINDTO => "",
+    ];
+
+    private function normalizeUri(string $uri): string {
+        return stripos($uri, 'unix://') === 0 ? $uri : strtolower($uri);
+    }
+
+    /** @inheritdoc */
+    public function checkout(string $uri, array $options = []): Promise {
+        $uri = $this->normalizeUri($uri);
+
+        unset($options[self::OP_IDLE_TIMEOUT]);
+        $options = array_merge($this->options, $options);
+
+        if (empty($this->sockets[$uri])) {
+            return $this->checkoutNewSocket($uri, $options);
+        }
+
+        foreach ($this->sockets[$uri] as $socketId => $socket) {
+            if (!$socket->isAvailable) {
+                continue;
+            } elseif (!\is_resource($socket->resource) || \feof($socket->resource)) {
+                unset($this->sockets[$uri][$socketId]);
+                continue;
+            } elseif (($bindTo = @\stream_context_get_options($socket->resource)['socket']['bindto'])) {
+                if ($bindTo !== $options[self::OP_BINDTO]) {
+                    continue;
+                }
+            }
+
+            $socket->isAvailable = false;
+
+            if (isset($socket->idleWatcher)) {
+                Loop::disable($socket->idleWatcher);
+            }
+
+            return new Success($socket->resource);
+        }
+
+        return $this->checkoutNewSocket($uri, $options);
+    }
+
+    private function checkoutNewSocket(string $uri, array $options): Promise {
+        return call(function () use ($uri, $options) {
+            $this->pendingCount[$uri] = ($this->pendingCount[$uri] ?? 0) + 1;
+
+            $rawSocket = yield rawConnect($uri, $options);
+            $socketId = (int) $rawSocket;
+
+            $socket = new SocketPoolStruct;
+            $socket->id = $socketId;
+            $socket->uri = $uri;
+            $socket->resource = $rawSocket;
+            $socket->isAvailable = false;
+            $socket->idleTimeout = $this->options[self::OP_IDLE_TIMEOUT];
+
+            $this->sockets[$uri][$socketId] = $socket;
+            $this->socketIdUriMap[$socketId] = $uri;
+
+            if (--$this->pendingCount[$uri] === 0) {
+                unset($this->pendingCount[$uri]);
+            }
+
+            return $rawSocket;
+        });
+    }
+
+    /** @inheritdoc */
+    public function clear($resource) {
+        $socketId = (int) $resource;
+
+        if (!isset($this->socketIdUriMap[$socketId])) {
+            throw new \Error(
+                sprintf('Unknown socket: %s', $resource)
+            );
+        }
+
+        $uri = $this->socketIdUriMap[$socketId];
+        $socket = $this->sockets[$uri][$socketId];
+
+        if ($socket->idleWatcher) {
+            Loop::cancel($socket->idleWatcher);
+        }
+
+        unset(
+            $this->sockets[$uri][$socketId],
+            $this->socketIdUriMap[$socketId]
+        );
+
+        if (empty($this->sockets[$uri])) {
+            unset($this->sockets[$uri]);
+        }
+    }
+
+    /** @inheritdoc */
+    public function checkin($resource) {
+        $socketId = (int) $resource;
+
+        if (!isset($this->socketIdUriMap[$socketId])) {
+            throw new \Error(
+                sprintf('Unknown socket: %s', $resource)
+            );
+        }
+
+        $uri = $this->socketIdUriMap[$socketId];
+
+        if (!is_resource($resource) || feof($resource)) {
+            $this->clear($resource);
+            return;
+        }
+
+        $socket = $this->sockets[$uri][$socketId];
+        $socket->isAvailable = true;
+
+        if (isset($socket->idleWatcher)) {
+            Loop::enable($socket->idleWatcher);
+        } else {
+            $socket->idleWatcher = Loop::delay($socket->idleTimeout, function () use ($socket) {
+                $this->clear($socket->resource);
+            });
+
+            Loop::unreference($socket->idleWatcher);
+        }
+    }
+
+    /**
+     * Gets the number of outstanding checkout requests.
+     *
+     * @param string $uri A URI as passed to `checkout()`.
+     *
+     * @return int
+     */
+    public function getPendingCount(string $uri): int {
+        $uri = $this->normalizeUri($uri);
+
+        return $this->pendingCount[$uri] ?? null;
+    }
+
+    /**
+     * Gets the number of currently checked out sockets.
+     *
+     * @param string $uri A URI as passed to `checkout()`.
+     *
+     * @return int
+     */
+    public function getCheckoutCount(string $uri): int {
+        $uri = $this->normalizeUri($uri);
+
+        return \count($this->sockets[$uri] ?? []);
+    }
+
+    /** @inheritdoc */
+    public function setOption(string $option, $value) {
+        switch ($option) {
+            case self::OP_CONNECT_TIMEOUT:
+                $this->options[self::OP_CONNECT_TIMEOUT] = (int) $value;
+                break;
+
+            case self::OP_IDLE_TIMEOUT:
+                $this->options[self::OP_IDLE_TIMEOUT] = (int) $value;
+                break;
+
+            case self::OP_BINDTO:
+                $this->options[self::OP_BINDTO] = $value;
+                break;
+
+            default:
+                throw new \Error(
+                    sprintf('Unknown option: %s', $option)
+                );
+        }
+    }
+}
--- lib/SocketPool.php
+++ lib/SocketPool.php
@@ -2,301 +2,73 @@
 
 namespace Amp\Socket;
 
-use Amp\Deferred;
-use Amp\Loop;
 use Amp\Promise;
-use Amp\Struct;
-use Amp\Success;
 
-/** @internal */
-class SocketPoolStruct {
-    use Struct;
-
-    public $id;
-    public $uri;
-    public $resource;
-    public $isAvailable;
-    public $idleWatcher;
-    public $idleTimeout;
-}
-
-class SocketPool {
-    const OP_HOST_CONNECTION_LIMIT = "amp.socket.socketpool.host-connection-limit";
+/**
+ * Allows pooling of connections for stateless protocols.
+ */
+interface SocketPool {
     const OP_IDLE_TIMEOUT = "amp.socket.socketpool.idle-timeout";
     const OP_CONNECT_TIMEOUT = "amp.socket.socketpool.connect-timeout";
     const OP_BINDTO = "amp.socket.socketpool.bindto";
-
-    private $sockets = [];
-    private $queuedSocketRequests = [];
-    private $socketIdUriMap = [];
-    private $pendingSockets = [];
-    private $options = [
-        self::OP_HOST_CONNECTION_LIMIT => 8,
-        self::OP_IDLE_TIMEOUT => 10000,
-        self::OP_CONNECT_TIMEOUT => 10000,
-        self::OP_BINDTO => "",
-    ];
-    private $needsRebind;
 
     /**
      * Checkout a socket from the specified URI authority.
      *
      * The resulting socket resource should be checked back in via `SocketPool::checkin()` once the calling code is
      * finished with the stream (even if the socket has been closed). Failure to checkin sockets will result in memory
-     * leaks and socket queue blockage.
+     * leaks and socket queue blockage. Instead of checking the socket in again, it can also be cleared.
      *
-     * @param string $uri A string of the form example.com:80 or 192.168.1.1:443
-     * @param array  $options
+     * @param string $uri A string of the form tcp://example.com:80 or tcp://192.168.1.1:443
+     * @param array  $options Array of options. OP_IDLE_TIMEOUT is ignored and has only effect on the pool itself.
      *
-     * @return \Amp\Promise Returns a promise that resolves to a socket once a connection is available
+     * @return Promise Returns a promise that resolves to a socket once a connection is available
      */
-    public function checkout(string $uri, array $options = []): Promise {
-        $uri = stripos($uri, 'unix://') === 0 ? $uri : strtolower($uri);
-        $options = $options ? array_merge($this->options, $options) : $this->options;
-
-        $socket = $this->checkoutExistingSocket($uri, $options);
-
-        return $socket ? new Success($socket) : $this->checkoutNewSocket($uri, $options);
-    }
-
-    private function checkoutExistingSocket($uri, $options) {
-        if (empty($this->sockets[$uri])) {
-            return null;
-        }
-
-        $needsRebind = false;
-
-        foreach ($this->sockets[$uri] as $socketId => $poolStruct) {
-            if (!$poolStruct->isAvailable) {
-                continue;
-            } elseif ($this->isSocketDead($poolStruct->resource)) {
-                unset($this->sockets[$uri][$socketId]);
-            } elseif (($bindToIp = @stream_context_get_options($poolStruct->resource)['socket']['bindto'])
-                && ($bindToIp === $options[self::OP_BINDTO])
-            ) {
-                $poolStruct->isAvailable = false;
-                if (isset($poolStruct->idleWatcher)) {
-                    Loop::disable($poolStruct->idleWatcher);
-                }
-                return $poolStruct->resource;
-            } elseif ($bindToIp) {
-                $needsRebind = true;
-            } else {
-                $poolStruct->isAvailable = false;
-                if (isset($poolStruct->idleWatcher)) {
-                    Loop::disable($poolStruct->idleWatcher);
-                }
-
-                return $poolStruct->resource;
-            }
-        }
-
-        $this->needsRebind = $needsRebind;
-
-        return null;
-    }
-
-    private function checkoutNewSocket($uri, $options) {
-        $needsRebind = $this->needsRebind;
-        $this->needsRebind = null;
-        $deferred = new Deferred;
-
-        if ($this->allowsNewConnection($uri, $options) || $needsRebind) {
-            $this->initializeNewConnection($deferred, $uri, $options);
-        } else {
-            $this->queuedSocketRequests[$uri][] = [$deferred, $uri, $options];
-        }
-
-        return $deferred->promise();
-    }
-
-    private function allowsNewConnection($uri, $options) {
-        $maxConnectionsPerHost = $options[self::OP_HOST_CONNECTION_LIMIT];
-
-        if ($maxConnectionsPerHost <= 0) {
-            return true;
-        }
-
-        $pendingCount = isset($this->pendingSockets[$uri]) ? $this->pendingSockets[$uri] : 0;
-        $existingCount = isset($this->sockets[$uri]) ? count($this->sockets[$uri]) : 0;
-        $totalCount = $pendingCount + $existingCount;
-
-        if ($totalCount < $maxConnectionsPerHost) {
-            return true;
-        }
-
-        return false;
-    }
-
-    private function initializeNewConnection(Deferred $deferred, $uri, $options) {
-        $this->pendingSockets[$uri] = isset($this->pendingSockets[$uri])
-            ? $this->pendingSockets[$uri] + 1
-            : 1;
-        $futureSocket = \Amp\Socket\rawConnect($uri, $options);
-        $futureSocket->onResolve(function ($error, $socket) use ($deferred, $uri, $options) {
-            if ($error) {
-                $deferred->fail($error);
-            } else {
-                $this->finalizeNewConnection($deferred, $uri, $socket, $options);
-            }
-        });
-    }
-
-    private function finalizeNewConnection(Deferred $deferred, $uri, $socket, $options) {
-        if (--$this->pendingSockets[$uri] === 0) {
-            unset($this->pendingSockets[$uri]);
-        }
-
-        $socketId = (int) $socket;
-        $poolStruct = new SocketPoolStruct;
-        $poolStruct->id = $socketId;
-        $poolStruct->uri = $uri;
-        $poolStruct->resource = $socket;
-        $poolStruct->isAvailable = false;
-        $poolStruct->idleTimeout = $options[self::OP_IDLE_TIMEOUT];
-        $this->sockets[$uri][$socketId] = $poolStruct;
-        $this->socketIdUriMap[$socketId] = $uri;
-        $deferred->resolve($poolStruct->resource);
-
-        if (empty($this->queuedSocketRequests[$uri])) {
-            unset($this->queuedSocketRequests[$uri]);
-        }
-    }
-
-    /**
-     * Remove the specified socket from the pool.
-     *
-     * @param resource $resource
-     *
-     * @return self
-     */
-    public function clear($resource): self {
-        $socketId = (int) $resource;
-        if (isset($this->socketIdUriMap[$socketId])) {
-            $uri = $this->socketIdUriMap[$socketId];
-            $this->unloadSocket($uri, $socketId);
-        }
-
-        return $this;
-    }
-
-    private function unloadSocket($uri, $socketId) {
-        if (!isset($this->sockets[$uri][$socketId])) {
-            return;
-        }
-
-        $poolStruct = $this->sockets[$uri][$socketId];
-        if ($poolStruct->idleWatcher) {
-            Loop::cancel($poolStruct->idleWatcher);
-        }
-        unset(
-            $this->sockets[$uri][$socketId],
-            $this->socketIdUriMap[$socketId]
-        );
-
-        if (empty($this->sockets[$uri])) {
-            unset($this->sockets[$uri][$socketId]);
-        }
-
-        if (!empty($this->queuedSocketRequests[$uri])) {
-            $this->dequeueNextWaitingSocket($uri);
-        }
-    }
-
-    private function dequeueNextWaitingSocket($uri) {
-        $queueStruct = current($this->queuedSocketRequests[$uri]);
-        /** @var Deferred $deferred */
-        list($deferred, $uri, $options) = $queueStruct;
-
-        if ($socket = $this->checkoutExistingSocket($uri, $options)) {
-            array_shift($this->queuedSocketRequests[$uri]);
-            $deferred->resolve($socket);
-            return;
-        }
-
-        if ($this->allowsNewConnection($uri, $options)) {
-            array_shift($this->queuedSocketRequests[$uri]);
-            $this->initializeNewConnection($deferred, $uri, $options);
-        }
-    }
+    public function checkout(string $uri, array $options = []): Promise;
 
     /**
      * Return a previously checked-out socket to the pool.
      *
-     * @param resource $resource
+     * @param resource $resource Raw socket resource.
      *
-     * @throws \Error on resource unknown to the pool
+     * @throws \Error If the provided resource is unknown to the pool.
      */
-    public function checkin($resource) {
-        $socketId = (int) $resource;
-
-        if (!isset($this->socketIdUriMap[$socketId])) {
-            throw new \Error(
-                sprintf('Unknown socket: %s', $resource)
-            );
-        }
-
-        $uri = $this->socketIdUriMap[$socketId];
-
-        if ($this->isSocketDead($resource)) {
-            $this->unloadSocket($uri, $socketId);
-        } else {
-            $this->finalizeSocketCheckin($uri, $socketId);
-        }
-    }
-
-    private function isSocketDead($resource) {
-        return !is_resource($resource) || feof($resource);
-    }
-
-    private function finalizeSocketCheckin($uri, $socketId) {
-        $poolStruct = $this->sockets[$uri][$socketId];
-        $poolStruct->isAvailable = true;
-
-        if (!empty($this->queuedSocketRequests[$uri])) {
-            $this->dequeueNextWaitingSocket($uri);
-        } elseif ($poolStruct->idleTimeout > 0) {
-            $this->initializeIdleTimeout($poolStruct);
-        }
-    }
-
-    private function initializeIdleTimeout(SocketPoolStruct $poolStruct) {
-        if (isset($poolStruct->idleWatcher)) {
-            Loop::enable($poolStruct->idleWatcher);
-        } else {
-            $poolStruct->idleWatcher = Loop::delay($poolStruct->idleTimeout, function () use ($poolStruct) {
-                $this->unloadSocket($poolStruct->uri, $poolStruct->id);
-            });
-            Loop::unreference($poolStruct->idleWatcher);
-        }
-    }
+    public function checkin($resource);
 
     /**
-     * Set socket pool option.
+     * Remove the specified socket from the pool.
      *
-     * @param string $option
-     * @param mixed  $value
+     * @param resource $resource Raw socket resource.
      *
-     * @throws \Error on unknown option
+     * @throws \Error If the provided resource is unknown to the pool.
      */
-    public function setOption(string $option, $value) {
-        switch ($option) {
-            case self::OP_HOST_CONNECTION_LIMIT:
-                $this->options[self::OP_HOST_CONNECTION_LIMIT] = (int) $value;
-                break;
-            case self::OP_CONNECT_TIMEOUT:
-                $this->options[self::OP_CONNECT_TIMEOUT] = (int) $value;
-                break;
-            case self::OP_IDLE_TIMEOUT:
-                $this->options[self::OP_IDLE_TIMEOUT] = (int) $value;
-                break;
-            case self::OP_BINDTO:
-                $this->options[self::OP_BINDTO] = $value;
-                break;
-            default:
-                throw new \Error(
-                    sprintf('Unknown option: %s', $option)
-                );
-        }
-    }
+    public function clear($resource);
+
+    /**
+     * Gets the number of outstanding checkout requests.
+     *
+     * @param string $uri A URI as passed to `checkout()`.
+     *
+     * @return int
+     */
+    public function getPendingCount(string $uri): int;
+
+    /**
+     * Gets the number of currently checked out sockets.
+     *
+     * @param string $uri A URI as passed to `checkout()`.
+     *
+     * @return int
+     */
+    public function getCheckoutCount(string $uri): int;
+
+    /**
+     * Set a socket pool option.
+     *
+     * @param string $option Option name, use the provided constants.
+     * @param mixed  $value Option value.
+     *
+     * @throws \Error If an unknown option is provided.
+     */
+    public function setOption(string $option, $value);
 }
