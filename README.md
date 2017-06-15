db3976a482864b2f2eb7dbd10e240854bbc261b7
Niklas Keller <me@kelunik.com>
2017-06-15 17:32:17 +0200

    Rewrite examples to new API

--- README.md
+++ README.md
@@ -7,36 +7,12 @@
 
 `amphp/socket` is a socket library for establishing and encrypting non-blocking sockets for [Amp](https://github.com/amphp/amp).
 
-**Required PHP Version**
-
-- PHP 7.0+
-
-**Installation**
+## Installation
 
 ```bash
 composer require amphp/socket
 ```
 
-**Example**
+## Examples
 
-```php
-<?php // basic server, see examples/simple-http-server.php
-
-require __DIR__ . '/../vendor/autoload.php';
-
-use Amp\Loop;
-use Amp\Socket\ServerSocket;
-
-Loop::run(function () {
-    $server = Amp\Socket\listen("tcp://127.0.0.1:0", function (ServerSocket $socket) {
-        list($ip, $port) = explode(":", $socket->getRemoteAddress());
-
-        $body = "Hey, your IP is {$ip} and your local port used is {$port}.";
-        $bodyLength = \strlen($body);
-
-        yield $socket->end("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: {$bodyLength}\r\n\r\n{$body}");
-    });
-
-    echo "Listening for new connections on " . $server->getAddress() . " ...\n";
-});
-```
+You can find several examples in the [`./examples`](./examples) directory.
--- examples/simple-http-server.php
+++ examples/simple-http-server.php
@@ -1,13 +1,21 @@
-<?php // basic server
+<?php // basic (and dumb) HTTP server
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// This is a very simple HTTP server that just prints a message to each client that connects.
+// It doesn't check whether the client sent an HTTP request.
+
+// You might notice that your browser opens several connections instead of just one, even when only making one request.
+
 use Amp\Loop;
 use Amp\Socket\ServerSocket;
+use function Amp\asyncCoroutine;
 
 Loop::run(function () {
-    $server = Amp\Socket\listen("tcp://127.0.0.1:0", function (ServerSocket $socket) {
+    $clientHandler = asyncCoroutine(function (ServerSocket $socket) {
         list($ip, $port) = explode(":", $socket->getRemoteAddress());
+
+        echo "Accepted connection from {$ip}:{$port}." . PHP_EOL;
 
         $body = "Hey, your IP is {$ip} and your local port used is {$port}.";
         $bodyLength = \strlen($body);
@@ -15,5 +23,12 @@ Loop::run(function () {
         yield $socket->end("HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: {$bodyLength}\r\n\r\n{$body}");
     });
 
-    echo "Listening for new connections on " . $server->getAddress() . " ...\n";
+    $server = Amp\Socket\listen("127.0.0.1:0");
+
+    echo "Listening for new connections on " . $server->getAddress() . " ..." . PHP_EOL;
+    echo "Open your browser and visit http://" . $server->getAddress() . "/" . PHP_EOL;
+
+    while ($socket = yield $server->accept()) {
+        $clientHandler($socket);
+    }
 });
