20f6e517c1fb45298c4bd99dfbbefa0e6aea4f8e
Joel Wurtz <jwurtz@jolicode.com>
2017-12-18 01:22:09 +0100

    Remove null byte, due to a bug in php returning null for socket name in that case

--- test/SocketTest.php
+++ test/SocketTest.php
@@ -25,14 +25,15 @@ class SocketTest extends TestCase {
     }
 
     public function testLocalAddressAsUnixSocket() {
+        @unlink(__DIR__ . '/socket.sock');
         $socket = \socket_create(AF_UNIX, SOCK_STREAM, 0);
         // Prepending the path with a null byte will abstract the socket and remove it when not used anymore see http://man7.org/linux/man-pages/man7/unix.7.html
-        \socket_bind($socket, "\0". __DIR__ . '/socket.sock');
+        \socket_bind($socket,  __DIR__ . '/socket.sock');
 
         socket_set_nonblock($socket);
         socket_listen($socket);
 
-        $clientSocket = new Socket\ClientSocket(stream_socket_client("unix://\0" . __DIR__ . '/socket.sock'));
+        $clientSocket = new Socket\ClientSocket(stream_socket_client("unix://" . __DIR__ . '/socket.sock'));
 
         self::assertEquals($clientSocket->getRemoteAddress(), $clientSocket->getLocalAddress());
     }
