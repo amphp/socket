828fd1c43e2cec3270a4935fd4bc7ec34c3e7b6e
Niklas Keller <me@kelunik.com>
2019-05-30 15:10:41 +0200

    Turn StaticSocketPool into StaticConnector

---
+++ src/StaticConnector.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Amp\Socket;
+
+use Amp\CancellationToken;
+use Amp\Promise;
+
+/**
+ * Connector that connects to a statically defined URI instead of the URI passed to the connect() call.
+ */
+final class StaticConnector implements Connector
+{
+    private $uri;
+    private $connector;
+
+    public function __construct(string $uri, Connector $connector)
+    {
+        $this->uri = $uri;
+        $this->connector = $connector;
+    }
+
+    public function connect(string $uri, ConnectContext $context = null, CancellationToken $token = null): Promise
+    {
+        return $this->connector->connect($this->uri);
+    }
+}
---
+++ test/StaticConnectorTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Amp\Socket\Test;
+
+use Amp\PHPUnit\TestCase;
+use Amp\Socket\Connector;
+use Amp\Socket\StaticConnector;
+
+class StaticConnectorTest extends TestCase
+{
+    public function testConnect(): void
+    {
+        /** @var Connector $underlyingConnector */
+        $underlyingConnector = $this->prophesize(Connector::class);
+        $staticSocketPool = new StaticConnector('override-uri', $underlyingConnector->reveal());
+
+        $expected = new \Amp\LazyPromise(function () {
+            // nothing
+        });
+
+        $underlyingConnector->connect('override-uri')->shouldBeCalled()->willReturn($expected);
+
+        $returned = $staticSocketPool->connect('test-url');
+
+        self::assertSame($expected, $returned);
+    }
+}
