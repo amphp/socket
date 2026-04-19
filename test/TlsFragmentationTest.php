dadb63c5d3179fd83803e29dfeac27350e619314
Aaron Piotrowski <aaron@trowski.com>
2026-04-19 09:45:23 -0500

    Cleanup TLS test

--- test/TlsFragmentationTest.php
+++ test/TlsFragmentationTest.php
@@ -20,7 +20,6 @@ class TlsFragmentationTest extends AsyncTestCase
 
         // Proxy to apply chunking of single bytes
         async(function () use ($proxyServer, $server): void {
-            /** @var Socket\ResourceSocket $proxyClient */
             while ($proxyClient = $proxyServer->accept()) {
                 async(function () use ($proxyClient, $server): void {
                     $proxyUpstream = Socket\connect($server->getAddress());
@@ -32,11 +31,9 @@ class TlsFragmentationTest extends AsyncTestCase
         });
 
         async(function () use ($server): void {
-            /** @var Socket\ResourceSocket $client */
             while ($client = $server->accept()) {
                 async(function () use ($client): void {
                     $client->setupTls();
-                    $this->assertInstanceOf(Socket\ResourceSocket::class, $client);
                     $this->assertSame('Hello World', $this->read($client, 11));
                     $client->write('test');
                     $client->close();
@@ -49,15 +46,15 @@ class TlsFragmentationTest extends AsyncTestCase
                 ->withCaFile(__DIR__ . '/tls/amphp.org.crt')
         );
 
-        /** @var Socket\ResourceSocket $client */
         $client = Socket\connect($proxyServer->getAddress(), $context);
         $client->setupTls();
         $client->write('Hello World');
 
+        delay(0.1); // Delay until after the server has closed the connection.
+
         self::assertSame('test', $this->read($client, 4));
 
-        delay(0.1);
-
+        $client->close();
         $proxyServer->close();
         $server->close();
     }
@@ -88,6 +85,6 @@ class TlsFragmentationTest extends AsyncTestCase
             }
         }
 
-        throw new \RuntimeException("Stream ended prior to {$minLength} bytes being read.");
+        throw new \RuntimeException("Stream ended prior to {$minLength} bytes being read; received: {$buffer}");
     }
 }
